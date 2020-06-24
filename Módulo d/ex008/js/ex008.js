0b3c4eb8973e9722bff984bae2ed3d4ec02e8e57
Jone Bulande <JoneBulande@gmail.com>
2020-06-24 21:10:33 +0200

    introdução a condições no javascript: condições simples

---
+++ Módulo d/ex008/js/ex008.js
@@ -0,0 +1,8 @@
+//var vel = Number(prompt("Qual é a volocidade?"));
+var vel = 10;
+console.log(`A volocidade do seu carro é ${vel}km/h`);
+
+if (vel > 60) {
+	console.log(`Voce ultrapassou a volocidade permitida. Multado!`);
+}
+console.log(`Dirija sempre usando cinto de segurança`);
