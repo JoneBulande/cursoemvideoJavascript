48d213178ef5888fe776dfaabcd97d8ee30aec55
Jone Bulande <JoneBulande@gmail.com>
2020-06-25 13:33:23 +0200

    Resolução do exercício/desafio ex009

--- Módulo d/desafio (exercícios)/js/ex009.js
+++ Módulo d/desafio (exercícios)/js/ex009.js
@@ -2,7 +2,7 @@ function verificar() {
 	var pais = document.querySelector("#pais").value;
 	var res = document.querySelector("#res");
 	if (pais == "Moçambique" && "moçambique") {
-		res.innerHTML = `Voce nasceu em ${pais}, voce é <i>Moçabicano</i>`;
+		res.innerHTML = `Voce nasceu em ${pais}, voce é <i>Moçambicano</i>`;
 	}else{
 		res.innerHTML = `Voce nasceu em ${pais}, voce é <b>extrangeiro</b>`;
 	}
