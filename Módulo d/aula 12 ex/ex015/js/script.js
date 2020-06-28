5c3e8ab3d133fd648c27fd016094583c020ebc65
Jone Bulande <JoneBulande@gmail.com>
2020-06-28 12:03:08 +0200

    Resolução do exercícios ex015

--- Módulo d/aula 12 ex/ex015/js/script.js
+++ Módulo d/aula 12 ex/ex015/js/script.js
@@ -1,17 +1,41 @@
-function carregar() {
-	var msg = document.getElementById('msg');
-	var img = document.getElementById('img');	
-	var hora = new Date().getHours();
-	msg.innerHTML = `Agora são ${hora} horas`;
-	if (hora  >= 0 && hora < 12 ) {
-		img.src = "img/manha.png";
-		document.body.style.background = "#e0b379";
-	}else if(hora  >= 12 && hora < 18 ){
-		img.src = "img/tarde.png";
-		document.body.style.background = "#be5d1d";
-	}else{
-		img.src = "img/noite.png";
-		document.body.style.background = "#484f55";
-	}
+function verificar() {
+	var ano = new Date().getFullYear(),
+		fAno = document.getElementById('txtAno').value,
+		res = document.querySelector("#res"),
+		idade = (ano - fAno);
+		
+		if (fAno.length != "" && fAno < ano && fAno.length >= 4) {
+			var fSex = document.getElementsByName("radsex"),
+				genero = "",
+				img = document.createElement("img");
+				img.setAttribute("id", "foto");
+				if (fSex[0].checked) {
+					genero = "Homem";
+					if (idade >= 0 && idade < 14) {
+						// Criaça
+						img.setAttribute("src", "img/crianca-homem.png");
+					}else if (idade < 23) {
+						img.setAttribute("src", "img/jovem-homem.png");
+					}else if (idade < 50) {
+						img.setAttribute("src", "img/adulto.png");
+					}else{
+						img.setAttribute("src", "img/idoso.png");
+					}
+				}else if (fSex[1]) {
+					genero = "Mulher";
+					if (idade >= 0 && idade < 14) {
+						img.setAttribute("src", "img/crianca-mulher.png");
+					}else if (idade < 23) {
+						img.setAttribute("src", "img/jovem-mulher.png");
+					}else if (idade < 50) {
+						img.setAttribute("src", "img/adulta.png");
+					}else{
+						img.setAttribute("src", "img/idosa.png");
+					}
+				}
+			res.innerHTML = `Detectamos ${genero} com ${idade} anos`;
+			res.appendChild(img);
+		}else{
+			alert("[erro]. verifique os dados e tente novamente.");
+	}	
 }
-
