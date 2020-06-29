c99eca52913dabdf93a77cd65f0daea51d4340ea
Jone Bulande <JoneBulande@gmail.com>
2020-06-29 12:45:49 +0200

    tentando resolver os exercícios sozinho com o console.log funciona

---
+++ Módulo e/execícios aula 14/ex016/2/script.js
@@ -0,0 +1,17 @@
+function contar() {
+	var n1 = Number(document.querySelector("#n1").value),
+		n2 = Number(document.querySelector("#n2").value),
+		n3 = Number(document.querySelector("#n3").value),
+		res = document.querySelector("#res");
+		if (n1 != " " && n1 != "") {
+			if (n2 != " " && n2 != "") {
+				if (n3 != " " && n3 != "") {
+					for(i=n1; i<=n2; i+=n3){
+						console.log(`valor: ${i}`);
+					}
+				}
+			}
+		}else{
+			alert("[erro], Prencha os campos corretamente e volte a tentar!");
+		}
+}
