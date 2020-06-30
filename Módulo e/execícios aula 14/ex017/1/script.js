0a09c2e8ad09ed67ef912b904ab8390bc3a3b6a7
Jone Bulande <JoneBulande@gmail.com>
2020-06-30 02:49:19 +0200

    Resolução do exercícios ex017

--- Módulo e/execícios aula 14/ex017/1/script.js
+++ Módulo e/execícios aula 14/ex017/1/script.js
@@ -7,9 +7,9 @@ function gerar() {
 	res.style.display = "block";;
 	res.innerHTML = "";
 	if (n1 != 0 && n1 != "") {
-			for(var i= 1; i<=12; i++){
-				res.innerHTML += `<laber>${n1} x ${i} = ${n1*i}<laberl> <br/>`;
-			}
+		for(var i= 1; i<=12; i++){
+			res.innerHTML += `<laber>${n1} x ${i} = ${n1*i}<laberl> <br/>`;
+		}
 	}else{
 		alert("[erro] - Preencha corretamente o campo e volte a tentar");
 	}
