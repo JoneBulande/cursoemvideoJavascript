ed11f43b858f7a85c41af92d2e433f96df8fb420
Jone Bulande <JoneBulande@gmail.com>
2020-07-01 13:46:38 +0200

    adicionei o codigo do Guanabara para a resolução do exercício ex016

--- Módulo e/execícios aula 14/ex016/2 Guanabara's code version/script.js
+++ Módulo e/execícios aula 14/ex016/2 Guanabara's code version/script.js
@@ -5,12 +5,17 @@ function ler() {
 	let res = document.getElementById("res");
 
 	if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+		res.innerHTML = "Impossivel contar!";
 		window.alert("[ERRO], faltam dados!");
 	} else {
 		res.innerHTML = "Contando: <br/>";
 		let i = Number(ini.value);
 		let	f = Number(fim.value);
 		let	p = Number(passo.value);
+		if (p<=0) {
+			window.alert("Passo inválido! considerando PASSO 1!");
+			p = 1;
+		}
 		if (i<f) {
 			//Contagem Crescente
 			for(let c = i; c <= f; c += p) {
