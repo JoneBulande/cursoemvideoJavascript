faabd844e312728b4e5c5c83d6dc4ce08c147133
Jone Bulande <JoneBulande@gmail.com>
2020-07-01 15:32:04 +0200

    melhorei o estilo das páginas

--- Módulo e/execícios aula 14/ex016/1 my code version/script.js
+++ Módulo e/execícios aula 14/ex016/1 my code version/script.js
@@ -4,6 +4,7 @@ function contar() {
 		n3 = Number(document.querySelector("#n3").value),
 		res = document.querySelector("#res");
 		if (n1!=0 && n2!=0 && n3!=0) {
+			res.innerHTML = "";
 			if (n1<n2) {
 				for(i = n1; i<=n2; i+=n3){
 					res.innerHTML += `${i} &#128073;`;
